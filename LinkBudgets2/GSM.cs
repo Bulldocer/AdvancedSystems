e0c9a80a666388bb6234e7d9d05b3d20ab771ce5
Jiorgon <navarrotrocal@hotmail.com>
2018-12-18 10:35:50 +0100

    Tag control and classes for each method

---
+++ LinkBudgets2/GSM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class GSM
+{
+    public float A1; //Power amplifier output power
+
+
+
+    public GSM()
+	{
+        GSM gsm = new GSM(a1, a2, a3, a4);
+        
+	}
+
+    public void CalculateA4() 
+    {
+
+    }
+}
---
+++ LinkBudgets2/WIMAX.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class WIMAX
+{
+	public WIMAX()
+	{
+	}
+}
