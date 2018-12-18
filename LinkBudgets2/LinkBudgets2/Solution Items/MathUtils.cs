292364a6b8a7505e4b291f682c8083dd71fd3f96
jorge alava <jar190@hotmail.com>
2018-12-18 10:58:11 +0100

    Change Classes into the project

---
+++ LinkBudgets2/LinkBudgets2/Solution Items/MathUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/// <summary>
+/// Helper class for math stuff
+/// </summary>
+public static class MathUtils
+{
+    /// <summary>
+    /// Calculate x to the power of 2, doing explicit casting
+    /// </summary>
+    /// <param name="x">X</param>
+    /// <returns></returns>
+    public static float Pow2Float(float x)
+    {
+        return (float)Math.Pow((double)x, (double)2.0f);
+    }
+
+    /// <summary>
+    /// Calculate 10 to the power of x, doing explicit casting
+    /// </summary>
+    /// <param name="x">X</param>
+    /// <returns></returns>
+    public static float TenPowXFloat(float x)
+    {
+        return (float)Math.Pow((double)10.0f, (double)x);
+    }
+
+    /// <summary>
+    /// Calculate base 10 log of x, doing explicit casting
+    /// </summary>
+    /// <param name="x">X</param>
+    /// <returns></returns>
+    public static float Log10Float(float x)
+    {
+        return (float)Math.Log10((double)x);
+    }
+}
---
+++ LinkBudgets2/LinkBudgets2/Solution Items/UMTS.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class UMTS
+{
+	public UMTS()
+	{
+	}
+}
